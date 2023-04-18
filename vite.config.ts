f740b404559be5a95f01151a182ecc48d7a433e4
qiuzhanghua <qiuzhanghua@icloud.com>
2023-04-18 09:52:30 +0800

    upgrade unocss -> 0.51.4

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,34 @@
-import { defineConfig } from "vite"
-import { svelte } from "@sveltejs/vite-plugin-svelte"
-import UnoCSS from "unocss/vite"
-import presetIcons from "@unocss/preset-icons"
-import presetUno from "@unocss/preset-uno"
-import { extractorSvelte } from "@unocss/core"
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import UnoCSS from "unocss/vite";
+import presetIcons from "@unocss/preset-icons";
+import presetUno from "@unocss/preset-uno";
+import extractorSvelte from "@unocss/extractor-svelte";
+import { presetAttributify, presetTypography} from "unocss";
 
 export default defineConfig({
   plugins: [
     UnoCSS({
-      extractors: [extractorSvelte],
+      extractors: [
+        extractorSvelte(),
+      ],
       shortcuts: [
         { logo: "i-logos-svelte-icon w-6em h-6em transform transition-800 hover:rotate-180" },
         { foo: "bg-yellow-400" },
-        { bar: "bg-green-400" },
+        { bar: "bg-green-400" }
       ],
       presets: [
+        presetAttributify(),
         presetUno(),
+        presetTypography(),
         presetIcons({
           extraProperties: {
             "display": "inline-block",
-            "vertical-align": "middle",
-          },
+            "vertical-align": "middle"
+          }
         }),
-      ],
+      ]
     }),
-    svelte(),
-  ],
+    svelte()
+  ]
 });
